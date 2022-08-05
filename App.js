d693e2e0ed2971f9e821c379e709bfb04268ea7b
Aminul Islam Tibro <aminultibro015@gmail.com>
2022-08-05 20:26:58 +0600

    registration page

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.page}>Login Page</Text>
+
+      <TextInput style={styles.username} placeholder="User Name"/>
+      <TextInput style={styles.username} placeholder="Full Name"/>
+      <TextInput style={styles.username} placeholder="Email"/>
+      <TextInput style={styles.username} placeholder="Password"/>
+      <Button style={{color:"red" , backgroundColor:"green"}} title="Sign Up"/>
+      
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
@@ -17,4 +26,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  page:{
+    fontSize:40,
+    fontWeight:700,
+    color:'blue'
+  },
+
+  username:{
+    marginTop: 15,
+    width: '50%',
+        backgroundColor: "white",
+        borderWidth: 1,
+        borderColor: 'grey',
+        padding: 10,
+        fontSize: 20,
+        marginBottom:25,
+  },
+
+  bt:{
+    display:"",
+    marginTop:20,
+    color: "secondary"
+  },
+  img:{
+    innerHeight:25
+  }
 });
